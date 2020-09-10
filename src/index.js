c34d975e2d1e2d579fadaab69e8e73b0b70aa91a
Coleman Lindquist <coleman.lindquist.32@gmail.com>
2020-09-09 23:19:33 -0600

    finished

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { createStore, applyMiddleware, combineReducers } from 'redux'
+import promiseMiddleware from 'redux-promise-middleware'
+import budgetReducer from './ducks/budgetReducer'
+import userReducer from './ducks/userReducer'
 
-ReactDOM.render(
-  <App />
-  , document.getElementById('root'));
+	
+const rootReducer = combineReducers({
+  budget: budgetReducer,
+  user: userReducer
+})
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+export default createStore(rootReducer, applyMiddleware(promiseMiddleware))
